78166af8f3377b9db90bdfaad777f1b2153cdbf6
sheyla astorga <sheylaastorga1998@gmail.com>
2023-11-09 17:50:06 -0300

    Agregue consigna del ejercicio 4A de trello

---
+++ js/Ejercicio4A.js
@@ -0,0 +1,3 @@
+// Ejercicio 4A: Generar números primos: Escribe una función que tome un número entero como argumento y devuelva un arreglo de todos los números primos menores o iguales a ese número.
+
+
