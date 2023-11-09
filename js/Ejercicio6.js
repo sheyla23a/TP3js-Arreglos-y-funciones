39d7eb2b10bb8e92da973b51f1b9cdd250aac8da
sheyla astorga <sheylaastorga1998@gmail.com>
2023-11-09 18:32:33 -0300

    Agregue consigna del ejercicio

---
+++ js/Ejercicio6.js
@@ -0,0 +1,7 @@
+// 6- Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, luego crear una función para calcular su perímetro y mostrarlo por pantalla.
+// La fórmula del perímetro  es p = 2*(a +b)
+// Ejemplo:
+// Input:
+// lado A = 24
+// lado B = 5
+// Output: 58
